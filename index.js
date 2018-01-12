4bfa2e74a83fc730eacac5c15ca8fee2d7c63fa5
eko prastyo <eko@softwareseni.com>
2018-01-12 09:54:07 +0700

    get query param, add public folder as assets, use middleware

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const express = require('express');
 
 const app = express();
 
+// middleware
+app.use((req, res, next) => {
+  next();
+});
+
 app.get('/', (req, res) => res.json({text: 'hello world'}));
+
+app.get('/page/:pageNumber', (req,res) => res.send(`Page no ${req.params.pageNumber}`));
+
+app.use(express.static('public'));
 
 app.listen(3000, () => console.log('Server jalan nih'));
 
